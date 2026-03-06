1614012ea7d9924bf7eb0061511cc00a509b00e3
stefannia <stefannia.castro@gmail.com>
2026-03-06 16:36:45 -0500

    prueba final de hotel Java

--- src/main/java/application/domain/Bedroom.java
+++ src/main/java/application/domain/Bedroom.java
@@ -1,116 +1,44 @@
 package application.domain;
 
-import java.util.List;
-
 public class Bedroom {
 
-   private int id;
-   private String room;
-   private String roomType;
-   private double price;
-   private boolean state;
+    private int id;
+    private String room;
+    private String roomType;
+    private double price;
+    private boolean state;
 
+    public Bedroom() {}
 
-
-   //constructors
-
-
-    public Bedroom(){}
-
-     public Bedroom(int id, String room, String roomType, double price,boolean state){
-
-
-         this.id = id;
-         this.room = room;
-         this.roomType = roomType;
-         this.price = price;
-         this.state = state;
-
-     }
-         //geters and setters
-
-          public int getId() {
-
-              return id;
-
-
-          }
-
-          public void setId(int id){
-              this.id=id;
-
-              }
-
-              public String getRoom(){
-                  return room;
-
-              }
-
-              public void setRoom(String Room){
-
-                      this.room=room;
-
-
-              }
-
-
-              public double getprice(){
-              return price;
-
-              }
-
-
-              public void setPrice(double price){
-                this.price = price;
-
-              }
-
-              public boolean getState(){
-               return state;
-
-              }
-
-              public void setState(double State){
-                  this.state=state;
-
-              }
-
-
-              //class methods
-
-    public Bedroom createUser(Bedroom bedroom){
-
-
-        return bedroom;
+    public Bedroom(int id, String room, String roomType, double price, boolean state) {
+        this.id = id;
+        this.room = room;
+        this.roomType = roomType;
+        this.price = price;
+        this.state = state;
     }
 
-
-    public Bedroom updateUser(Bedroom bedroom){
-
-
-        return bedroom;
+    public String getRoom() {
+        return room;
     }
 
-    public List<Bedroom> getbetroom(){
-        return null;
+    public void setRoom(String room) {
+        this.room = room;
     }
 
-    public Bedroom getbedroomById(int id){
-        return null;
+    public String getRoomType() {
+        return roomType;
     }
 
-    public void deleteBedroom(int id){
-
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
     }
 
+    public double getPrice() {
+        return price;
+    }
 
-
-
-
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
-
-
-
-
-
-
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,39 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import application.domain.Bedroom;
+import application.domain.service;
+import application.domain.guest;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Sistema del Hotel");
+
+        // Crear habitación
+        Bedroom room = new Bedroom();
+        room.setRoom("101");
+        room.setRoomType("Suite");
+        room.setPrice(120);
+
+        System.out.println("Habitación: " + room.getRoom());
+        System.out.println("Tipo: " + room.getRoomType());
+        System.out.println("Precio: " + room.getPrice());
+
+        // Crear servicio
+        service service = new service();
+        service.setName("Spa");
+        service.setPrice(50);
+
+        System.out.println("Servicio: " + service.getName());
+        System.out.println("Precio servicio: " + service.getPrice());
+
+        // Crear huésped
+        guest guest = new guest();
+        guest.setName("Juan");
+        guest.setLastName("Perez");
+
+        System.out.println("Huésped: " + guest.getName() + " " + guest.getLastName());
     }
 }
